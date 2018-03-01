e941423371eca95cda5902c936411d79998c98b2
pitipon <pitipon@gmail.com>
2018-03-01 20:49:07 +0800

    primary button .. change color

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -3,10 +3,19 @@ function initUpdateNavbarOnScroll() {
     window.addEventListener('scroll', () => {
       if (window.scrollY >= window.innerHeight) {
         navbar.classList.add('navbar-wagon-white');
+        document.querySelectorAll("a").forEach((a) => {
+          a.classList.remove('navbar-wagon-link')
+          a.classList.add('navbar-wagon-link2')
+        });
       } else {
         navbar.classList.remove('navbar-wagon-white');
+        document.querySelectorAll("a").forEach((a) => {
+          a.classList.add('navbar-wagon-link')
+          a.classList.remove('navbar-wagon-link2')
+        });
       }
     });
   }
+
   
   export { initUpdateNavbarOnScroll };
